469d726cd5edee93efa46e39ff739e1fefd229e4
nguyenthanhlam9621 <71130267+nguyenthanhlam9621@users.noreply.github.com>
2020-09-18 16:45:58 +0700

    lab4

---
+++ lab4/lab4ss8.4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float com=0,sales_amt;
+	char grade;
+	printf("Enter the sales amount:");
+	scanf("%f",&sales_amt);
+	fflush(stdin);
+	printf("\nEnter the Grade:");
+	scanf("%c",&grade);
+	
+	if(sales_amt>10000)
+	if(grade=='A')
+	com=sales_amt*0.1;
+	else
+	com=sales_amt*0.08;
+	else
+	com=sales_amt*0.05;
+	printf("\nCommission=%f",com);
+	return 0;
+}
