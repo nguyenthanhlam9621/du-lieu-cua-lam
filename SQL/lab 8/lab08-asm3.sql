b9c94c0b6566a593d2c0f5b0ac506031fdc3635d
nguyenthanhlam9621 <71130267+nguyenthanhlam9621@users.noreply.github.com>
2020-12-28 20:20:08 +0700

    lab8

---
+++ SQL/lab 8/lab08-asm3.sql
@@ -0,0 +1,118 @@
+﻿USE master 
+GO
+ 
+ IF EXISTS(SELECT*FROM sys.databases WHERE Name LIKE 'lab08_2')
+ DROP DATABASE lab08_2
+ GO
+
+ CREATE DATABASE lab08_2
+ GO
+
+ USE lab08_2
+ GO
+
+ CREATE TABLE Customers(
+ IDKhachhang int NOT NULL,
+ TenKhachHang nvarchar(50),
+ SoChungMinh varchar(20),
+ DiaChi varchar(20),
+ )
+ GO
+
+ ALTER TABLE Customers
+ ADD CONSTRAINT PK_CUS PRIMARY KEY (IDKhachhang)
+ GO
+
+  CREATE TABLE Services(
+ ServiceID int NOT NULL,
+ [Name] varchar(10),
+ [Description] varchar(50),
+ )
+ GO
+ ALTER TABLE Services
+ ADD CONSTRAINT PK_Ser PRIMARY KEY (ServiceID)
+ GO
+
+ CREATE TABLE Tel(
+ SoThueBao varchar(20) NOT NULL,
+ LoaiThueBao varchar(15),
+ NgayDangKy datetime,
+
+ IDKhachhang int,
+ ServiceID int
+ )
+ GO
+
+ ALTER TABLE Tel
+ ADD CONSTRAINT PK_Tel PRIMARY KEY (SoThueBao)
+ GO
+ ALTER TABLE Tel
+ ADD CONSTRAINT FK_Cus FOREIGN KEY (IDKhachhang) REFERENCES Customers(IDKhachhang)
+ GO
+  ALTER TABLE Tel
+ ADD CONSTRAINT FK_Ser FOREIGN KEY (ServiceID) REFERENCES Services(ServiceID)
+ GO
+
+
+
+
+ INSERT INTO Customers VALUES 
+ (1,N'N VAN A','123456789','Ha Noi'),
+ (2,N'N VAN B','223456789','Hai Phong'),
+ (3,N'N VAN C','323456789','Ha Noi')
+ GO
+
+ INSERT INTO Services VALUES
+ (1,'3G','Mang Binh Thuong'),
+ (2,'4G','Mang Nhanh'),
+ (3,'5G','Mang Sieu Nhanh')
+ GO
+ INSERT INTO Tel VALUES
+ ('0987654321','TRA TRUOC','01-01-2000',1,1),
+ ('0907654321','TRA SAU','12-12-2009',1,2),
+ ('097554321','TRA TRUOC','01-01-2001',2,2),
+ ('097634321','TRA TRUOC','01-01-2002',3,3)
+ GO
+ --4.1 Hiển thị toàn bộ thông tin của các khách hàng của công ty.
+ SELECT *FROM Customers
+ --4.2Hiển thị toàn bộ thông tin của các số thuê bao của công ty.
+ SELECT *FROM Tel
+ --5.1Hiển thị toàn bộ thông tin của thuê bao có số: 0123456789
+ SELECT *FROM Tel WHERE IDKhachhang IN (
+	SELECT IDKhachhang FROM Customers WHERE SoThueBao LIKE '0123456789')
+	GO
+--5.2Hiển thị thông tin về khách hàng có số CMTND: 123456789
+SELECT * FROM Customers WHERE SoChungMinh LIKE '123456789'
+--5.3Hiển thị các số thuê bao của khách hàng có số CMTND:123456789
+SELECT SoThueBao FROM Tel WHERE IDKhachhang IN (
+	SELECT IDKhachhang FROM Customers WHERE SoChungMinh LIKE '123456789')
+--5.4Liệt kê các thuê bao đăng ký vào ngày 12/12/09
+SELECT *FROM Tel WHERE NgayDangKy = '12-12-2009'
+--5.5Liệt kê các thuê bao có địa chỉ tại Hà Nội
+SELECT *FROM Tel WHERE IDKhachhang IN (
+	SELECT IDKhachhang FROM Customers WHERE DiaChi LIKE 'Ha Noi')
+--6.1Tổng số khách hàng của công ty.
+SELECT COUNT(IDKhachhang)AS[SO KHACH] FROM Customers
+--6.2Tổng số thuê bao của công ty.
+SELECT COUNT(SoThueBao)AS[So Luong Thue Bao] FROM Tel
+--6.3Tổng số thuê bào đăng ký ngày 12/12/09.
+SELECT COUNT(SoThueBao) AS [DK12-12-09] FROM Tel WHERE NgayDangKy = '12-12-2009'
+--6.4Hiển thị toàn bộ thông tin về khách hàng và thuê bao của tất cả các số thuê bao.
+SELECT *FROM Customers JOIN Tel ON Customers.IDKhachhang=Tel.IDKhachhang JOIN Services ON Tel.ServiceID=Services.ServiceID
+--7.1Viết câu lệnh để thay đổi trường ngày đăng ký là not null.
+ALTER TABLE Tel
+ALTER COLUMN NgayDangKy DATETIME NOT NULL
+GO
+--7.2Viết câu lệnh để thay đổi trường ngày đăng ký là trước hoặc bằng ngày hiện tại.
+ALTER TABLE Tel
+ADD CONSTRAINT ck_ngaydangky CHECK (NgayDangKy <= GETDATE())
+GO
+--7.3Viết câu lệnh để thay đổi số điện thoại phải bắt đầu 09
+ALTER TABLE Tel
+ADD CONSTRAINT ck_Sothuebao CHECK (SoThueBao LIKE '09%')
+GO
+SELECT * From tel
+--7.4Viết câu lệnh để thêm trường số điểm thưởng cho mỗi số thuê bao.
+
+--8.1 Đặt chỉ mục (Index) cho cột Tên khách hàng của bảng chứa thông tin khách hàng
+CREATE INDEX IX_Customers ON Customers(TenKhachHang)
---
+++ SQL/lab 8/lab08.sql
@@ -0,0 +1,110 @@
+﻿USE master
+GO
+ 
+IF EXISTS(SELECT * FROM sys.databases WHERE Name LIKE 'lab08_1')
+DROP DATABASE lab08_1
+GO
+
+CREATE DATABASE lab08_1
+GO 
+
+USE lab08_1
+GO
+
+CREATE TABLE Company(
+ComID int NOT NULL,
+ComName varchar(10),
+Tel varchar(20),
+[Address] varchar(10)
+)
+GO
+
+CREATE TABLE Category(
+CategoryID int PRIMARY KEY,
+CatName nvarchar(20)
+)
+GO
+
+CREATE TABLE Product(
+ProID int IDENTITY Primary KEY,
+[Name] nvarchar(20),
+[Description] nvarchar(40),
+Unit nvarchar(20),
+Price money,
+Qty int,
+ComID int,
+CategoryID int,
+)
+GO
+
+--3:Nhap gia tri cho cac bang
+INSERT INTO Company VALUES(123,'ASUS','0983232','TAWAN')
+INSERT INTO Company VALUES(234,'NOKIA','0872121','FINLAND')
+INSERT INTO Company VALUES(345,'SAMSUNG','0761010','KOREA')
+GO
+
+INSERT INTO Category VALUES(1,N'ĐIỆN THOẠI')
+INSERT INTO Category VALUES(2,N'MÁY TÍNH')
+INSERT INTO Category VALUES(3,N'MÁY IN')
+GO
+
+INSERT INTO Product VALUES(N'Máy Tính T450',N'Máy Nhập Cũ',N'CHIẾC',1000,10,123,1)
+INSERT INTO Product VALUES(N'Điện Thoại Nokia5670',N'Điện thoại đang hot',N'CHIẾC',200,200,234,2)
+INSERT INTO Product VALUES(N'Máy In Samsung 450',N'Máy in đang loại bình',N'CHIẾC',100,10,345,3)
+GO
+--4.1: Hien thi thong tin cong ty
+SELECT * FROM Company
+--4.2:hien thi thong tin san pham
+SELECT * FROM Product
+
+--5.1:Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên
+SELECT * FROM Company ORDER BY ComName DESC
+--5.2:Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần.
+SELECT * FROM Product ORDER BY Price DESC
+--5.3:Hiển thị thông tin của hãng Asus.
+SELECT *FROM Company WHERE ComName LIKE 'ASUS'
+--5.4:Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho
+SELECT * FROM Product WHERE Qty < 11
+--5.5:Liệt kê danh sách sản phẩm của hãng Asus
+SELECT *FROM Product WHERE ComID IN (
+	SELECT ComID FROM Company WHERE ComName LIKE 'ASUS')
+
+--6.1:Số hãng sản phẩm mà cửa hàng có.
+SELECT COUNT(ComName) AS[SoLuongHang] FROM Company
+--6.2:Số mặt hàng mà cửa hàng bán.
+SELECT COUNT(ProID) AS[Somathang] FROM Product
+--6.3:Tổng số loại sản phẩm của mỗi hãng có trong cửa hàng.
+SELECT COUNT(ProID) AS[LoaispcuahangASUS] FROM Product WHERE ComID IN (
+	SELECT ComID FROM Company WHERE ComName LIKE 'ASUS')
+SELECT COUNT(ProID) AS[LoaispcuahangNOKIA] FROM Product WHERE ComID IN (
+	SELECT ComID FROM Company WHERE ComName LIKE 'NOKIA')
+SELECT COUNT(ProID) AS[LoaispcuahangSAMSUNG] FROM Product WHERE ComID IN (
+	SELECT ComID FROM Company WHERE ComName LIKE 'SAMSUNG')
+--6.4:Tổng số đầu sản phẩm của toàn cửa hàng.
+SELECT COUNT(ProID) AS [DAUSP] FROM Product
+
+--7.1:Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0).
+ALTER TABLE Product
+ADD CONSTRAINT dk_price CHECK (Price>0)
+GO
+--7.2: Viết câu lệnh để thay đổi số điện thoại phải bắt đầu bằng 0.
+ALTER TABLE Company
+ADD CONSTRAINT dk_Tel CHECK (Tel LIKE '0%')
+GO
+--7.3:Viết các câu lệnh để xác định các khóa ngoại và khóa chính của các bảng.
+ALTER TABLE Company 
+ADD CONSTRAINT pk_Com PRIMARY KEY (ComID)
+GO
+
+ALTER TABLE Product
+ADD CONSTRAINT Fk_Company  FOREIGN KEY (ComID) REFERENCES Company(ComID)
+GO
+
+ALTER TABLE Product
+ADD CONSTRAINT Fk_Category FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
+GO
+
+
+--8.1Thiết lập chỉ mục (Index) cho các cột sau: Tên hàng và Mô tả hàng để tăng hiệu suất truy vấn dữ liệu từ 2 cột này
+CREATE INDEX IX_Description ON Product(Description)
+CREATE INDEX IX_Name ON Product(Name)
