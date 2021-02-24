02d17c0ce9c2af8fbb103a6482dfce2b8e901d2d
nguyenthanhlam9621 <71130267+nguyenthanhlam9621@users.noreply.github.com>
2021-02-24 17:49:31 +0700

    b1 ky 2

---
+++ ky 2/bai 1/PrintPattern.java
@@ -0,0 +1,37 @@
+public class PrintPattern {
+    public static void main(String[] args){
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * *");
+        System.out.println();
+        System.out.println("* * * * *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+        System.out.println();
+        System.out.println("* * * * *");
+        System.out.println(" *     * ");
+        System.out.println("  *   *  ");
+        System.out.println("   * *   ");
+        System.out.println("    *    ");
+        System.out.println();
+        System.out.println("    *    ");
+        System.out.println("* *   * *");
+        System.out.println("   * *   ");
+        System.out.println("  *   *  ");
+        System.out.println(" *     * ");
+        System.out.println();
+        System.out.println("         '  '");
+        System.out.println("          -- ");
+        System.out.println("         (oo)");
+        System.out.println("  /========//");
+        System.out.println(" / || @@ ||  ");
+        System.out.println("*  ||----||  ");
+        System.out.println("   VV    VV  ");
+        System.out.println("   ''    ''  ");
+
+    }
+}
