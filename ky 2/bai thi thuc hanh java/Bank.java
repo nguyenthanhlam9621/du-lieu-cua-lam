b82189f3bd5287234ef9893febb416cf031ba05c
nguyenthanhlam9621 <71130267+nguyenthanhlam9621@users.noreply.github.com>
2021-03-15 19:01:03 +0700

    baithi java

---
+++ ky 2/bai thi thuc hanh java/Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+    private double balance;
+    private double rate;
+
+    public Bank(double balance,double rate){
+        this.balance=balance;
+        this.rate=rate;
+    }
+    public double calculateInterest(){
+        return balance*(rate/1200);
+    }
+}
